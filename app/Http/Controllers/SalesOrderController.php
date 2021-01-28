22b969f2899bdf732f4a9896b0f4efc8ec5b7473
princejose2020 <prjo526@ess.ais.ac.nz>
2021-01-28 15:25:27 +1300

    Updated the SalesOrderController, added SalesOrder listing function with pagination

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class SalesOrderController extends Controller
 {
     private $baseUrl;
-    private $salesOrderCollection = [];
+    private $itemPerPage = 10;
 
     /**
      * Create a new SalesOrderController instance.
@@ -26,11 +27,25 @@ class SalesOrderController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $response = Http::withOptions(['verify' => false,])
-            ->get($this->baseUrl."controllers/api/picking/listoso.php");
-        
-        dd($response->json());
+        //Calling external API
+        $response = Http::withOptions(['verify' => false,])->get($this->baseUrl . "controllers/api/picking/listoso.php");
+
+        //Check the response status, If failed return empty array
+        if ($response->failed()) {
+            return [];
+        }
+
+        //Current page
+        $page = isset($request->page) ? $request->page : 1;
+
+        $startingPoint = ($page * $this->itemPerPage) - $this->itemPerPage;
+        $responseArray = array_slice($response->json(), $startingPoint, $this->itemPerPage, true);
+
+        //Create a LengthAwarePaginator instance
+        $result = new LengthAwarePaginator($responseArray, count($response->json()), $this->itemPerPage, $page);
+
+        return $result;
     }
 }
