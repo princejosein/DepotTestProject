56ebf80acc5985970206e50e87395aa1d88a7b59
princejose2020 <prjo526@ess.ais.ac.nz>
2021-01-28 15:48:04 +1300

    Created AppHome, AppToolbar and AppFooter vue components, Install vue-rooter package

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <app-toolbar></app-toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import AppToolbar from './AppToolbar.vue';
+import AppFooter from './AppFooter.vue';
+export default {
+    components:{AppToolbar, AppFooter}
+}
+</script>
+
+<style>
+
+</style>
